585b1e4011b949afb1a9a93db6ba5ea4b5b94d75
sujit471 <108291389+sujit471@users.noreply.github.com>
2024-06-25 23:45:58 +0545

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'provider/photo_provider.dart';
 import 'screens/Gridview.dart';
+import 'package:lottie/lottie.dart';
+import 'package:animated_splash_screen/animated_splash_screen.dart';
 
 void main() {
   runApp(MyApp());
@@ -18,8 +20,46 @@ class MyApp extends StatelessWidget {
         theme: ThemeData(
           primarySwatch: Colors.blue,
         ),
-        home: PhotoGridScreen(),
+        home: SplashScreen(),
       ),
     );
   }
 }
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedSplashScreen(
+      splash: Stack(
+        children: [
+          Center(
+            child: SizedBox(
+              width: 300,
+              height: 300,
+              child: Lottie.asset(
+                'animation/gallery.json',
+                repeat: true,
+                reverse: true,
+                animate: true,
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+          Center(
+            child: Image.asset(
+              'image/logo.png',
+              width: 150,
+              height: 150,
+            ),
+          ),
+        ],
+      ),
+      nextScreen:PhotoGridScreen(),
+      splashTransition: SplashTransition.fadeTransition,
+      duration: 3000,
+      backgroundColor: Colors.black,
+    );
+  }
+}
+
